422ad118e97385c68f4fddf53e513ebe81dabdab
ualter.junior@gmail.com <ualter.junior@gmail.com>
2016-09-26 23:05:20 +0200

    .

--- src/main/java/br/ujr/scorecard/model/orcamento/OrcamentoDAOHibernate.java
+++ src/main/java/br/ujr/scorecard/model/orcamento/OrcamentoDAOHibernate.java
@@ -90,22 +90,24 @@ public class OrcamentoDAOHibernate extends HibernateDaoSupport implements Orcame
 	public Set<Passivo> listPassivosOrcamento(ContaCorrente contaCorrente, long referenciaInicial, long referenciaFinal, String nivelConta) {
 		try {
 			
-			if ( nivelConta.endsWith("0") ) {
-				nivelConta = nivelConta.substring(0, 2);
+			String nivelContaFilhas = nivelConta + ".%";
+			if ( checkContaMestre(nivelConta) ) {
+				nivelConta = prepareForContaMestre(nivelConta);
+				nivelContaFilhas = nivelConta + "%";
+				nivelConta = nivelConta + ".0";
 			}
-			nivelConta += "%";
 			
 			Set<Passivo> passivos = new HashSet<Passivo>();
-			String[] params = new String[]{"dataIni","dataFim","contaCorrente","nivelConta"};
-			Object[] values = new Object[]{referenciaInicial,referenciaFinal,contaCorrente.getId(),nivelConta};
-			
+			String[] params = new String[]{"dataIni","dataFim","contaCorrente","nivelConta", "nivelContaFilhas"};
+			Object[] values = new Object[]{referenciaInicial,referenciaFinal,contaCorrente.getId(),nivelConta, nivelContaFilhas};
 			
 			StringBuffer strQuery = new StringBuffer();
 			strQuery.delete(0, strQuery.length());
 			strQuery.append("  select P, P.passivo from Parcela as P ");
 			strQuery.append("   where P.referencia >= :dataIni and P.referencia <= :dataFim");
 			strQuery.append("   and P.passivo.contaCorrente.id = :contaCorrente");
-			strQuery.append("   and P.passivo.conta.nivel like :nivelConta ");
+			strQuery.append("   and (P.passivo.conta.nivel = :nivelConta OR P.passivo.conta.nivel like :nivelContaFilhas) ");
+			values = new Object[]{referenciaInicial,referenciaFinal,contaCorrente.getId(),nivelConta, nivelContaFilhas};
 			List listPassivos = this.getHibernateTemplate().findByNamedParam(strQuery.toString(), params, values);
 			for(Iterator iterator = listPassivos.iterator(); iterator.hasNext();) {
 				Object[]   row      = (Object[]) iterator.next();
@@ -186,7 +188,6 @@ public class OrcamentoDAOHibernate extends HibernateDaoSupport implements Orcame
 				Object[]   row      = (Object[]) iterator.next();
 				String     nivel  = (String)row[0];
 				BigDecimal total    = (BigDecimal)row[1];
-				
 				if ( somaPorNivelContaNoPeriodo.containsKey(nivel) ) {
 					total = total.add((BigDecimal)somaPorNivelContaNoPeriodo.get(nivel));
 					somaPorNivelContaNoPeriodo.put(nivel, total);
@@ -204,21 +205,9 @@ public class OrcamentoDAOHibernate extends HibernateDaoSupport implements Orcame
 				 * Nivel da Conta Contábil do Orçamento
 				 */
 				String nivelContaOrcamento = orcamento.getContaAssociada().getNivel();
-				
 				BigDecimal jahRealizado = null;
-				/**
-				 * Se nivel da conta contábil do orçamento for um nível de raiz, exemplo: "4.0", cortar o zero para buscar com as iniciais a partir do ponto, 
-				 * assim... será buscado contas que iniciem com "4.", e não com "4.0". 
-				 * Os alvos neste caso poderão ser "4.1", "4.2", "4.1.1", "4.3.2.1", etc.
-				 * 
-				 * Caso não seja raiz, seja por exemplo "4.2", nada é feito, busca-se filhas de "4.2", como "4.2.1", "4.2.1.2", etc.
-				 * 
-				 *   ...a condicional abaixo sobre o teste de tamanho (lenght <= 3) 
-				 *      é por causa de contas filhas que terminam com zero como 5.10 ou 6.10, etc. e não são contas "mestres", como a 5.0 ou 6.0
-				 */
-				if ( nivelContaOrcamento.endsWith("0") && nivelContaOrcamento.length() <= 3) {
-					nivelContaOrcamento = nivelContaOrcamento.substring(0, 2);
-				}
+				
+				nivelContaOrcamento = prepareForContaMestre(nivelContaOrcamento);
 				
 				/**
 				 * Iteração sobre os totais realizados por nível de conta contábil procurando por contas contábeis de nível descendente da do orçamento
@@ -227,7 +216,7 @@ public class OrcamentoDAOHibernate extends HibernateDaoSupport implements Orcame
 					/**
 					 * Inicial do Nível do Total Realizado é igual a do Orçamento em questão ?  (Ex:  "4.1.2.1" começa com "4.1" ?) 
 					 */
-					if ( key.startsWith(nivelContaOrcamento) ) {
+					if ( key.equals(nivelContaOrcamento) || key.startsWith(nivelContaOrcamento + ".") ) {
 						/**
 						 * Caso positivo faz parte do realizado deste Orçamento, soma-se ao total realizado do mesmo.
 						 */
@@ -247,5 +236,26 @@ public class OrcamentoDAOHibernate extends HibernateDaoSupport implements Orcame
 			log.error(e.getMessage(),e);
 			throw e;
 		}
+	}
+
+	/**
+	 * Se nivel da conta contábil do orçamento for um nível de raiz, exemplo: "4.0", cortar o zero para buscar com as iniciais a partir do ponto, 
+	 * assim... será buscado contas que iniciem com "4.", e não com "4.0". 
+	 * Os alvos neste caso poderão ser "4.1", "4.2", "4.1.1", "4.3.2.1", etc.
+	 * 
+	 * Caso não seja raiz, seja por exemplo "4.2", nada é feito, busca-se filhas de "4.2", como "4.2.1", "4.2.1.2", etc.
+	 * 
+	 *   ...a condicional abaixo sobre o teste de tamanho (lenght <= 3) 
+	 *      é por causa de contas filhas que terminam com zero como 5.10 ou 6.10, etc. e não são contas "mestres", como a 5.0 ou 6.0
+	 */
+	private boolean checkContaMestre(String nivelContaOrcamento) {
+		return  ( nivelContaOrcamento.endsWith("0") && nivelContaOrcamento.length() <= 3); 
+	}
+	private String prepareForContaMestre(String nivelContaOrcamento) {
+		
+		if ( checkContaMestre(nivelContaOrcamento) ) {
+			nivelContaOrcamento = nivelContaOrcamento.substring(0, 1);
+		}
+		return nivelContaOrcamento;
 	}
 }
