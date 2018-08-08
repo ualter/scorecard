97e6ffda62dfa2458608fa7cd1518af516c6123e
Ualter Azambuja Junior <ualter.junior@gmail.com>
2018-08-08 08:19:32 +0200

    .

--- mysql/v_7.0.sql
+++ mysql/v_7.0.sql
@@ -1,4 +1,33 @@
+
+# Disable Itau
+UPDATE `banco` SET `IS_ATIVO` = 'F' WHERE (`ID` = '60');
+
+# Has Cheque Conta Corrente
+ALTER TABLE `conta_corrente` 
+ADD COLUMN `HAS_CHEQUE` VARCHAR(1) NULL DEFAULT 'T' AFTER `ORDEM`;
+
+# Insert Banc Sabadell
+INSERT INTO `banco` (`NOME`, `DIAVENCIMENTOVISA`, `DIAVENCIMENTOMASTERCARD`, `IS_ATIVO`) VALUES ('Banc Sabadell', '29', '29', 'T');
+
+set @bancoSabadellId := 64;
+# Insert Cuenta Corriente Banc Sabadell
+insert INTO `conta_corrente` (`BANCO_ID`, `DESCRICAO`, `NUMERO`, `ORDEM`, `HAS_CHEQUE`) VALUES (@bancoSabadellId, 'Banc Sabadell (Euros)', '00810133430001678371', '3', 'T');
+
+# Keys Banco (Execute one at a time)
+select 64 INTO @keyBB;
+select 66 INTO @keySantander;
+select 67 INTO @keyItau;
+select 69 INTO @keyDB;
+select 70 INTO @keyBancSabadell;
+
+select @keyBB;
+select @keySantander;
+select @keyItau;
+select @keyDB;
+select @keyBancSabadell;
+
 # Create Table cartao_contratado
+DROP TABLE IF EXISTS `cartao_contratado`;
 CREATE TABLE `cartao_contratado` (
   `ID` int(10) NOT NULL AUTO_INCREMENT,
   `OPERADORA` int(10) NOT NULL,
@@ -7,38 +36,28 @@ CREATE TABLE `cartao_contratado` (
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `scorecard_dev`.`cartao_contratado` (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA BB', '64');
-INSERT INTO `scorecard_dev`.`cartao_contratado` (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron BB', '64');
-INSERT INTO `scorecard_dev`.`cartao_contratado` (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard BB', '64');
-INSERT INTO `scorecard_dev`.`cartao_contratado` (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA Santander', '66');
-INSERT INTO `scorecard_dev`.`cartao_contratado` (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron Santander', '66');
-INSERT INTO `scorecard_dev`.`cartao_contratado` (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard Santander', '66');
-INSERT INTO `scorecard_dev`.`cartao_contratado` (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA Itaú', '67');
-INSERT INTO `scorecard_dev`.`cartao_contratado` (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron Itaú', '67');
-INSERT INTO `scorecard_dev`.`cartao_contratado` (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard Itaú', '67');
-INSERT INTO `scorecard_dev`.`cartao_contratado` (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA DB', '69');
-INSERT INTO `scorecard_dev`.`cartao_contratado` (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron DB', '69');
-INSERT INTO `scorecard_dev`.`cartao_contratado` (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard DB', '69');
-INSERT INTO `scorecard_dev`.`cartao_contratado` (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA BS', '70');
-INSERT INTO `scorecard_dev`.`cartao_contratado` (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron BS', '70');
-INSERT INTO `scorecard_dev`.`cartao_contratado` (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard BS', '70');
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA BB',          @keyBB);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron BB', @keyBB);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard BB',    @keyBB);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA Santander',          @keySantander);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron Santander', @keySantander);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard Santander',    @keySantander);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA Itaú',          @keyItau);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron Itaú', @keyItau);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard Itaú',    @keyItau);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA DB',          @keyDB);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron DB', @keyDB);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard DB',    @keyDB);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA BS',          @keyBancSabadell);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron BS', @keyBancSabadell);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard BS',    @keyBancSabadell);
 
-# Has Cheque Conta Corrente
-ALTER TABLE `scorecard_dev`.`conta_corrente` 
-ADD COLUMN `HAS_CHEQUE` VARCHAR(1) NULL DEFAULT 'T' AFTER `ORDEM`;
 
-# Disable Itau
-UPDATE `scorecard_dev`.`banco` SET `IS_ATIVO` = 'F' WHERE (`ID` = '60');
 
-# Insert Banc Sabadell
-INSERT INTO `scorecard_dev`.`banco` (`NOME`, `DIAVENCIMENTOVISA`, `DIAVENCIMENTOMASTERCARD`) VALUES ('Banc Sabadell', '29', '29', 'T');
 
-# Insert Cuenta Corriente Banc Sabadell
-DECLARE keyBanc INT;
-SET @keyBanc = (select id from banco where Nome = 'Banc Sabadell');
-INSERT INTO `scorecard_dev`.`conta_corrente` (`BANCO_ID`, `DESCRICAO`, `NUMERO`, `ORDEM`) VALUES (@keyBanc, 'Banc Sabadell (Euros)', '00810133430001678371', '3');
 
-UPDATE `scorecard_dev`.`conta_corrente` SET `HAS_CHEQUE` = 'F' WHERE (`ID` = '70');
+
+UPDATE `conta_corrente` SET `HAS_CHEQUE` = 'F' WHERE (`ID` = '70');
 
 # Change cartao table structure
 ALTER TABLE `scorecard_dev`.`cartao` 
