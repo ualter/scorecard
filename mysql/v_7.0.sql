375d1d7146cb41bba7a2a40bf87f44266c96c4eb
Ualter Azambuja Junior <ualter.junior@gmail.com>
2018-08-08 10:20:05 +0200

    .

--- mysql/v_7.0.sql
+++ mysql/v_7.0.sql
@@ -1,30 +1,46 @@
+# Insert Banc Sabadell
+INSERT INTO `banco` (`NOME`, `DIAVENCIMENTOVISA`, `DIAVENCIMENTOMASTERCARD`, `IS_ATIVO`) VALUES ('Banc Sabadell', '29', '29', 'T');
+
+select ban.ID as 'Banco_ID' INTO @bancoIdSabadell from banco ban where ban.NOME = 'Banc Sabadell';
+select @bancoIdSabadell;
+
+# Insert Conta Corriente Sabadell
+INSERT INTO `conta_corrente` (`BANCO_ID`, `DESCRICAO`, `NUMERO`, `ORDEM`) VALUES (@bancoIdSabadell, 'Banc Sabadell', '000', '3');
+
+
+# Keys Conta Corrente Bancos (Execute one at a time)
+select cc.ID as 'CC_ID', ban.ID as 'Banco_ID' INTO @contaCorrenteIdBB, @bancoIdBB 
+from conta_corrente cc inner join banco ban on ban.id = cc.banco_id where ban.NOME = 'Banco do Brasil' and cc.NUMERO = '14.868-7';
+
+select cc.ID as 'CC_ID', ban.ID as 'Banco_ID' INTO @contaCorrenteIdSantander, @bancoIdSantander 
+from conta_corrente cc inner join banco ban on ban.id = cc.banco_id where ban.NOME = 'Santander';
+
+select cc.ID as 'CC_ID', ban.ID as 'Banco_ID' INTO @contaCorrenteIdItau, @bancoIdItau
+from conta_corrente cc inner join banco ban on ban.id = cc.banco_id where ban.NOME = 'Itaú';
+
+select cc.ID as 'CC_ID', ban.ID as 'Banco_ID' INTO @contaCorrenteIdDeutsche, @bancoIdDeutsche 
+from conta_corrente cc inner join banco ban on ban.id = cc.banco_id where ban.NOME = 'Deutsche Bank';
+
+select cc.ID as 'CC_ID', ban.ID as 'Banco_ID' INTO @contaCorrenteIdSabadell, @bancoIdSabadell 
+from conta_corrente cc inner join banco ban on ban.id = cc.banco_id where ban.NOME = 'Banc Sabadell';
+
+select @bancoIdBB, @contaCorrenteIdBB;
+select @bancoIdItau, @contaCorrenteIdItau;
+select @bancoIdSantander, @contaCorrenteIdSantander;
+select @bancoIdDeutsche, @contaCorrenteIdDeutsche;
+select @bancoIdSabadell, @contaCorrenteIdSabadell;
+
 
 # Disable Itau
-UPDATE `banco` SET `IS_ATIVO` = 'F' WHERE (`ID` = '60');
+UPDATE `banco` SET `IS_ATIVO` = 'F' WHERE (`ID` = @bancoIdBB );
 
 # Has Cheque Conta Corrente
 ALTER TABLE `conta_corrente` 
 ADD COLUMN `HAS_CHEQUE` VARCHAR(1) NULL DEFAULT 'T' AFTER `ORDEM`;
 
-# Insert Banc Sabadell
-INSERT INTO `banco` (`NOME`, `DIAVENCIMENTOVISA`, `DIAVENCIMENTOMASTERCARD`, `IS_ATIVO`) VALUES ('Banc Sabadell', '29', '29', 'T');
+# Update Cuenta Corriente Banc Sabadell
+UPDATE `conta_corrente` SET `HAS_CHEQUE` = 'F' WHERE (`ID` = @bancoIdSabadell);
 
-set @bancoSabadellId := 64;
-# Insert Cuenta Corriente Banc Sabadell
-insert INTO `conta_corrente` (`BANCO_ID`, `DESCRICAO`, `NUMERO`, `ORDEM`, `HAS_CHEQUE`) VALUES (@bancoSabadellId, 'Banc Sabadell (Euros)', '00810133430001678371', '3', 'T');
-
-# Keys Banco (Execute one at a time)
-select 64 INTO @keyBB;
-select 66 INTO @keySantander;
-select 67 INTO @keyItau;
-select 69 INTO @keyDB;
-select 70 INTO @keyBancSabadell;
-
-select @keyBB;
-select @keySantander;
-select @keyItau;
-select @keyDB;
-select @keyBancSabadell;
 
 # Create Table cartao_contratado
 DROP TABLE IF EXISTS `cartao_contratado`;
@@ -36,32 +52,35 @@ CREATE TABLE `cartao_contratado` (
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA BB',          @keyBB);
-INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron BB', @keyBB);
-INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard BB',    @keyBB);
-INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA Santander',          @keySantander);
-INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron Santander', @keySantander);
-INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard Santander',    @keySantander);
-INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA Itaú',          @keyItau);
-INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron Itaú', @keyItau);
-INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard Itaú',    @keyItau);
-INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA DB',          @keyDB);
-INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron DB', @keyDB);
-INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard DB',    @keyDB);
-INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA BS',          @keyBancSabadell);
-INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron BS', @keyBancSabadell);
-INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard BS',    @keyBancSabadell);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA BB',          @contaCorrenteIdBB);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron BB', @contaCorrenteIdBB);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard BB',    @contaCorrenteIdBB);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA Santander',          @contaCorrenteIdSantander);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron Santander', @contaCorrenteIdSantander);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard Santander',    @contaCorrenteIdSantander);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA Itaú',          @contaCorrenteIdItau);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron Itaú', @contaCorrenteIdItau);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard Itaú',    @contaCorrenteIdItau);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA DB',          @contaCorrenteIdDeutsche);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron DB', @contaCorrenteIdDeutsche);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard DB',    @contaCorrenteIdDeutsche);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('0', 'VISA BS',          @contaCorrenteIdSabadell);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('1', 'VISA Electron BS', @contaCorrenteIdSabadell);
+INSERT INTO cartao_contratado (`OPERADORA`, `NOME`, `CONTA_CORRENTE_ID`) VALUES ('2', 'Mastercard BS',    @contaCorrenteIdSabadell);
 
-
-
-
-
-
-UPDATE `conta_corrente` SET `HAS_CHEQUE` = 'F' WHERE (`ID` = '70');
+# Check It
+SELECT cc.*, ccr.DESCRICAO, ban.NOME
+FROM cartao_contratado cc
+INNER JOIN conta_corrente ccr ON ccr.id = cc.conta_corrente_id
+INNER JOIN banco ban ON ban.id = ccr.banco_id;
 
 # Change cartao table structure
-ALTER TABLE `scorecard_dev`.`cartao` 
-ADD COLUMN `CARTAO_CONTRATADO_ID` INT(10) NULL AFTER `CONTA_CORRENTE_ID`;
+ALTER TABLE `cartao` 
+ADD COLUMN `CARTAO_CONTRATADO_ID` INT(10) NULL AFTER `OPERADORA`;
 
 # Create Stored Procedure cartaoContratado_vs_cartaoPassivo and execute it...
 call cartaoContratado_vs_cartaoPassivo();
+
+# Check it
+SELECT c.*, p.historico, cc.DESCRICAO, cc.NUMERO FROM cartao c INNER JOIN passivo p on p.id = c.passivo_id
+inner join conta_corrente cc on p.conta_corrente_id = cc.id;
